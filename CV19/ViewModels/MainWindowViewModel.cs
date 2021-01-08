65d2534c7d2730cbbd36c4a2b0a7b803659c7ee7
ilyazov49@gmail.com <ilyazov49@gmail.com>
2021-01-08 15:56:57 +0300

    Тестирование работы модели-представления главного окна

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,32 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    public class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
-        
+        private string _title = "Анализ статиcтики CV19";
+
+        #region Заголовок окна
+
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set<string>(ref _title, value);
+            #region
+            //{
+            //    //if (!Equals(_title, value)) return;
+            //    //_title = value;
+            //    //OnPropertyChanged("Title");
+            //    Set<string>(ref _title,value);
+            //}
+            #endregion
+        } 
+        #endregion
+
+
+
+
+
     }
 }
